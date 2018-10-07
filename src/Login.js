b864b1ebaf5c49dc7c60cac3e396b0f4850d7a34
Brady <vanderbannond@gmail.com>
2018-10-06 18:03:24 -0700

    added bloomID verification snackbars

--- src/Login.js
+++ src/Login.js
@@ -4,11 +4,22 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { TextField, Button } from '@material-ui/core'
 import abi from './AccountRegistry.js'
-// let ABI = JSON.stringify(abi)
+import SnackBar from './Snackbar'
 
 const Web3 = require('web3')
-const currentProvider = '0xa4262058bc8a85274eaed34a2033f1d193f9f16a'
 
+// const signature = web3.eth.accounts.sign(web3.utils.utf8ToHex("Hello, Bloom!"), address);
+// AccountRegistry.methods.createNewAccount(address).send({
+//   from: address
+// },
+//   function (err, res) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//   }
+// })
+//
 // if (typeof web3 !== 'undefined') {
 // //   web3 = new Web3(currentProvider);
 // // } else {
@@ -17,7 +28,6 @@ const currentProvider = '0xa4262058bc8a85274eaed34a2033f1d193f9f16a'
 
 //test connection to rinkeby test net and query for balance of random known account, OK
 const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/"));
-var account1= "0x9795a539f682923Bb7055e8a7b43fA7b6005C763"; web3.eth.getBalance(account1).then(res => console.log(res))
 
 //BLOOM ID - create account registry object from contract on Rinkeby, logs OK
 console.log('BLOOM CONTRACT');
@@ -25,13 +35,20 @@ let AccountRegistry = new web3.eth.Contract(abi, '0xfbc650e80d6796dff3101457eac4
 console.log(AccountRegistry);
 
 //Calling method to check if wallet address is registered in BLOOM ID smart contract returning true or false, OK
-AccountRegistry.methods.addressBelongsToAccount("0x82f92383afb67c42a7635690f2eb4db408be2656").call(function (err, res) {
+
+
+//Find Bloom ID by registered wallet address
+AccountRegistry.methods.accountIdForAddress("0x82f92383afb67c42a7635690f2eb4db408be2656").call(function (err, res) {
   if (err) {
     console.log(err);
   } else {
     console.log(res);
   }
 })
+
+const BloomVerifiedAccount = '0x82f92383afb67c42a7635690f2eb4db408be2656'
+const BloomUnregisteredAccount = '0xa4262058bc8a85274eaed34a2033f1d193f9f16a'
+
 
 
 const styles = theme => ({
@@ -46,7 +63,7 @@ const styles = theme => ({
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200,
+    width: 300,
   },
   dense: {
     marginTop: 19,
@@ -63,18 +80,55 @@ class Login extends Component {
 
   state = {
     name: '',
-    email: ''
+    email: '',
+    success: false,
+    error: false
   }
+
+  componentDidMount() {
+
+  // Check if Web3 has been injected by the browser (MetaMask).
+  // (since 'web3' is global, we need to use 'window')
+  if (window.web3 && window.web3.currentProvider.isMetaMask) {
+    window.web3.eth.getAccounts((error, accounts) => {
+
+      // Do whatever you need to.
+      this.setState({wallet: accounts[0]});
+    });
+  } else {
+    console.log('MetaMask account not detected :(');
+  }
+}
 
   handleChange = name => event => {
     this.setState({ [name]: event.target.value })
   }
 
+  handleSubmit = () => {
+    const address = this.state.wallet
+    console.log('SUBMIT', address);
+    AccountRegistry.methods.addressBelongsToAccount(BloomVerifiedAccount).call().then((res, err) => {
+      if (err) {
+        console.log(err);
+        this.setState({ error: true, message: "Error validating Bloom account" })
+      } else if (res === false ){
+        this.setState({ error: true, message: "Error validating Bloom account" })
+      } else if (res === true) {
+        this.setState({ success: true, message: "Bloom ID verified!" })
+      }
+    })
+  }
+
+
+
+
   render() {
     const { classes } = this.props;
+    const { success, error, message } = this.state
 
     return (
         <form className={classes.container} noValidate autoComplete='off'>
+          <SnackBar success={success} error={error} message={message}/>
           <TextField
           id="standard-name"
           label="Name"
@@ -91,7 +145,7 @@ class Login extends Component {
         onChange={this.handleChange('email')}
         margin="normal"
       />
-      <Button variant="contained" color="secondary" className={classes.button}>
+      <Button variant="contained" color="secondary" className={classes.button} onClick={this.handleSubmit}>
         Submit
       </Button>
         </form>
